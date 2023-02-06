0d4e45bcdc5c196ae13add22e818a23c93abfcb7
iritmaximus <aaro.koivukangas@gmail.com>
2023-02-06 11:16:34 +0200

    feat: changed the project structure and added new routes

---
+++ flaskr/user.py
@@ -0,0 +1,2 @@
+def generate_token_with_username():
+    pass
--- flaskr/views.py
+++ flaskr/views.py
@@ -1,22 +1,45 @@
+from flask import render_template, request
+
 from flaskr import app
 from flaskr.database import db, test_db, create_db
 
 
 @app.route("/")
 def test_backend():
-    return "This is the working backend"
+    return render_template("home.html")
 
 
-@app.route("/db")
-def test_db_route():
-    return test_db()
+# TODO show 3 games with most times submitted
+@app.route("/games")
+def get_games():
+    return "List of games"
 
 
-@app.route("/login")
+# TODO show 3 courses with most activity
+@app.route("/courses")
+def get_courses():
+    return "List of courses"
+
+
+# TODO show 3 newest times submitted
+@app.route("/times")
+def get_times():
+    return "List of times"
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login to the site"
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        return "Todo POST request"
 
 
 @app.route("/init")
 def initialize_db():
     return create_db()
+
+
+@app.route("/db")
+def test_db_route():
+    return test_db()
